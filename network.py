c3190314ea31af8857f5397948ae4f82b8cf82ab
PhilippeRambaud <phiram2306@gmail.com>
2021-10-13 01:09:09 +0200

    pickle to handle send rcv

--- network.py
+++ network.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 class Network:
     def __init__(self):
@@ -6,22 +7,22 @@ class Network:
         self.server = "192.168.1.12"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
 
-    def get_pos(self):
-        return self.pos
+    def get_p(self):
+        return self.p
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048*100))
         except:
             pass
 
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048*100))
         except socket.error as e:
             print(e)
 
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 from _thread import *
-import sys
+from player import Player
+import pickle
 
 server = "192.168.1.12"  # server running on that machine
 port = 5555
@@ -16,39 +17,30 @@ except socket.error as e:
 s.listen(2)
 print("Waiting for a connection, Server Started!")
 
-
-def read_pos(str):  # "45,46" -> (45,46)
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-def make_pos(tup):  # (45,46) -> "45,46"
-    return str(tup[0]) + "," + str(tup[1])
-
-
-pos = [(0, 0), (100, 100)]  # hold positions of players
+players = [Player(0, 0, 50, 50, (255, 0, 0)), Player(100, 100, 50, 50, (0, 0, 255))]
 
 # threaded to let many connections
 # while game (other functions) still running
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:  # to receive some data
-            data = read_pos(conn.recv(2048).decode())  # larger the size = larger time to recv data
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048*100))  # larger the size = larger time to recv data
+            player[player] = data
 
             if not data:
                 print("Disconnected")
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
                 print("Received: ", data)
                 print("Sending: ", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))  # encode info before send to server
+            conn.sendall(pickle.dumps(reply))  # encode info before send to server
         except:
             break
 
