6401b43aeb9eeb948d93ff77acb810413272f235
PhilippeRambaud <phiram2306@gmail.com>
2021-10-13 11:49:04 +0200

    Part 9 - Online Rock Paper Scissors - GOOD

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 from _thread import *
 from player import Player
 import pickle
+from game import Game
 
 server = "192.168.1.12"  # server running on that machine
 port = 5555
@@ -14,45 +15,65 @@ try:
 except socket.error as e:
     str(e)
 
-s.listen(2)
+s.listen()
 print("Waiting for a connection, Server Started!")
 
-players = [Player(0, 0, 50, 50, (255, 0, 0)),
-           Player(100, 100, 50, 50, (0, 0, 255))]
+connected = set()
+games = {}
+id_count = 0
 
 # threaded to let many connections
 # while game (other functions) still running
-def threaded_client(conn, player):
-    conn.send(pickle.dumps(players[player]))
+def threaded_client(conn, p, game_id):
+    global id_count
+    conn.send(str.encode(str(p)))
+
     reply = ""
     while True:
-        try:  # to receive some data
-            data = pickle.loads(conn.recv(2048))  # larger the size = larger time to recv data
-            players[player] = data
+        try:
+            data = conn.recv(2048*2).decode()
 
-            if not data:
-                print("Disconnected")
-                break
-            else:
-                if player == 1:
-                    reply = players[0]
+            if game_id in games:
+                game = games[game_id]
+
+                if not data:
+                    break
                 else:
-                    reply = players[1]
-                print("Received: ", data)
-                print("Sending: ", reply)
+                    if data == "reset":
+                        game.reset()
+                    elif data != "get":
+                        game.play(p, data)
 
-            conn.sendall(pickle.dumps(reply))  # encode info before send to server
+                    reply = game
+                    conn.sendall(pickle.dumps(reply))
+            else:
+                break
         except:
             break
 
     print("Lost connection")
+    try:
+        del games[game_id]
+        print("Closing Game ", game_id)
+    except:
+        pass
+    id_count -= 1
     conn.close()
 
 
-current_player = 0
 while True:  # wait to grab connections
     conn, addr = s.accept()
     print("Connected to:", addr)
 
-    start_new_thread(threaded_client, (conn, current_player))
-    current_player += 1
+    id_count += 1
+    p = 0
+    game_id = (id_count - 1) // 2  # every 2 people increase game_id by 1
+    if id_count % 2 == 1:  # wait till new player
+        games[game_id] = Game(game_id)
+        print("Creating a new game...")
+    else:  # ready to launch game
+        games[game_id].ready = True
+        p = 1
+
+
+    start_new_thread(threaded_client, (conn, p, game_id))
